891c83d0579538a80bb12a486a5544c4e2fd4afd
Ahmad Ibrahim <ahmad.ib04@gmail.com>
2021-05-27 10:15:36 +0200

    aaa

--- src/git/Test.java
+++ src/git/Test.java
@@ -3,7 +3,11 @@ package git;
 public class Test {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Hello, World!");
+=======
+		System.out.println("Test from ahmad9100");
+>>>>>>> 1aa085c8bcd71859ea6d09bd0c839aeb7f467b69
 	}
 
 }
