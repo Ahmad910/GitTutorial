6520fdb6be9548297faf056b29efaf97f4be5bfc
Ahmad Ibrahim <ahmad.ib04@gmail.com>
2021-05-27 10:18:08 +0200

    merging

--- src/git/Test.java
+++ src/git/Test.java
@@ -3,11 +3,9 @@ package git;
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hello, World!");
-=======
-		System.out.println("Test from ahmad9100");
->>>>>>> 1aa085c8bcd71859ea6d09bd0c839aeb7f467b69
+
+		System.out.println("Hello, Worldddd!");
+
 	}
 
 }
